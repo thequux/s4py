6a777fb49fca2db4f11ccae7d05f1c28a77ae00c
TQ Hirsch <thequux@upstandinghackers.com>
2014-09-10 20:07:40 +0200

    Now decodes headers, schemas, and table info from a simdata file

---
+++ lib/s4py/simdata.py
@@ -0,0 +1,160 @@
+# At some point, I'll have schema'd objects lazily loaded from the
+# TDesc files. I'm not there yet. Thus, this module's interface is
+# likely to change *substantially* over the coming weeks.
+
+from collections import namedtuple
+from . import fnv1
+import contextlib
+import struct
+
+class FormatException(Exception):
+    pass
+
+_dword = struct.Struct("=I")
+
+class BReader:
+    def __init__(self, bstr):
+        self.raw = bstr
+        self._off = 0
+    def __getitem__(self, index):
+        return bstr[index]
+    def seek(self, off):
+        assert off < len(self.raw)
+        self.off = off
+
+    @property
+    def off(self):
+        return self._off
+    @off.setter
+    def off(self, val):
+        assert val <= len(self.raw) # <= so that you can point one past the end
+        self._off = val
+    
+    def get_off32(self):
+        """Read an offset relative to the current position; returns the absolute offset"""
+        off = self.off
+        res = self.get_int32()
+        if res == -0x80000000:
+            return None
+        else:
+            return off + res
+    def get_int32(self):
+        res = int.from_bytes(self.raw[self.off:self.off+4], "little", signed=True)
+        self.off += 4
+        return res
+    def get_uint32(self):
+        res = int.from_bytes(self.raw[self.off:self.off+4], "little", signed=False)
+        self.off += 4
+        return res
+
+    def get_int16(self):
+        res = int.from_bytes(self.raw[self.off:self.off+2], "little", signed=True)
+        self.off += 2
+        return res
+    def get_uint16(self):
+        res = int.from_bytes(self.raw[self.off:self.off+2], "little", signed=False)
+        self.off += 2
+        return res
+
+    def get_string(self):
+        """Read a null-terminated string"""
+        endOff = self.raw.index(b'\0', self.off)
+        res = self.raw[self.off:endOff]
+        self.off = endOff + 1
+        return res
+        
+    def get_relstring(self):
+        """Read a string from the next offset, read as an off32"""
+        off = self.get_off32()
+        if off is not None:
+            with self.at(off):
+                return self.get_string()
+        else:
+            return None
+
+    @contextlib.contextmanager
+    def at(self, posn):
+        """Temporarily read from another place
+
+        This is intended to be used like:
+        with reader.at(offset):
+           # read some stuff
+        """
+        saved = self.off
+        try:
+            self.off = posn
+            yield
+        finally:
+            self.off = saved
+
+        
+
+class SimData:
+
+    _TableData = namedtuple("_TableData", "name schema data_type row_size row_pos row_count")
+    _Schema = namedtuple("_Schema", "name schema_hash size columns")
+    _SchemaColumn = namedtuple("_SchemaColumn", "name data_type flags offset schema_pos")
+    def __init__(self):
+        pass
+
+    def fromBytes(self, bstr):
+        f = BReader(bstr)
+        if bstr[0:4] != b'DATA':
+            raise FormatException("This is not a valid simdata file")
+        f.off = 4
+        
+        version = f.get_uint32()
+        tablePos = f.get_off32()
+        numTables = f.get_int32()
+        schemaPos = f.get_off32()
+        numSchemas = f.get_int32()
+
+        f.off = tablePos
+        tables = []
+        schemas = {}
+        for _ in range(numTables):
+            print("Reading table at %x" %( f.off,))
+            tables.append(self._readTable(f))
+        
+        f.off = schemaPos
+        for _ in range(numSchemas):
+            off = f.off
+            schemas[off] = self._readSchema(f)
+
+        return (tables, schemas)
+        
+
+    def _readTable(self, f):
+        # f is a BReader
+        name = f.get_relstring()
+        nameHash = f.get_uint32()
+        probedHash = fnv1.fnv1((name or b"").lower(), 32)
+        assert probedHash == nameHash, ("%08x != %08x (%r)" % (probedHash, nameHash, name))
+        schemaPos = f.get_off32()
+        dataType = f.get_uint32()
+        rowSize = f.get_uint32()
+        rowOffset = f.get_off32()
+        rowCount = f.get_uint32()
+        return self._TableData(name, schemaPos, dataType, rowSize, rowOffset, rowCount)
+    
+    def _readSchema(self, f):
+        # f is a BReader
+        name = f.get_relstring()
+        nameHash = f.get_uint32()
+        assert fnv1.fnv1((name or b"").lower(), 32) == nameHash
+        schemaHash = f.get_uint32()
+        schemaSize = f.get_uint32()
+        columnPos = f.get_off32()
+        numColumns = f.get_uint32()
+
+        columns = []
+        with f.at(columnPos):
+            for _ in range(numColumns):
+                cName = f.get_relstring()
+                cNameHash = f.get_uint32()
+                cDataType = f.get_uint16()
+                cFlags = f.get_uint16()
+                cOffset = f.get_uint32()
+                cSchemaPos = f.get_off32()
+                columns.append(self._SchemaColumn(cName, cDataType, cFlags, cOffset, cSchemaPos))
+        return self._Schema(name, schemaHash, schemaSize, tuple(columns)) # Tuplifying the columns results in less work for the GC
