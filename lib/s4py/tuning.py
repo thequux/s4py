37b73cd1ec6ea67dbe47e0c9987441518b4910b4
TQ Hirsch <thequux@upstandinghackers.com>
2014-09-23 23:39:23 +0200

    Sketched out types for int and tuple

--- lib/s4py/tuning.py
+++ lib/s4py/tuning.py
@@ -11,6 +11,10 @@ class TuningType(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def binary_representer(self, val_list):
+        # Emitter is an collector of output tables. 
+        
+        # TODO: Think about how to collect val_list across all
+        # instances of this type and how to return references
         """Return an function that insert all of the objects in val_list into
         a binary stream
 
@@ -20,6 +24,43 @@ class TuningType(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def represent_yaml(self, dumper, obj):
         """Convert obj to YAML nodes"""
+
+class TuningListType(TuningType):
+    def __init__(self, inner_type=None, **kwargs):
+        super().__init__(inner_type=inner_type, **kwargs)
+        self.inner_type = inner_type
+    def validate_data(self, proposal):
+        if not isinstance(proposal, list) and not isinstance(proposal, tuple):
+            return False
+        for _ in proposal:
+            if not self.inner_type.validate_data(_):
+                return False
+        else:
+            return True
+    def binary_representer(self, val_list):
+        pass
+    def represent_yaml(self, dumper, obj):
+        return dumper.represent_sequence('tag:yaml.org,2002:seq', obj)
+
+def TuningTupleType(TuningType):
+    def __init__(self, members=None, **kwargs):
+        # Inner type must be a dict
+        super.__init__(members=members, **kwargs)
+        self.members = members
+    def validate_data(self, proposal):
+        if not isinstance(proposal, dict):
+            return False
+        for key in proposal:
+            if key not in self.members:
+                return False
+            if not self.members[key].validate_data(proposal[key]):
+                return False
+        else:
+            return True
+    def represent_yaml(self, dumper, obj):
+        return dumper.represent_mapping('tag:yaml.org,2002:map', obj)
+
+# TODO: Represent atomic type, localized strings, etc
 
 class Tunable:
     # TO BE SPECIFIED: How are descriptions, bounds, etc, represented?
@@ -38,5 +79,3 @@ class Tunable:
     @abc.abstractmethod
     def type(self):
         """Return the TuningType that is expected"""
-
-# TODO: Implement list, tuple, and atomic types
